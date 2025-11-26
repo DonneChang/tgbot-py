5d85a8c9ee7b3246cbb4dc1009419f2423b24d73
DonneChang <luckydays.donne@gmail.com>
2025-11-27 01:05:05 +0800

    更新

--- schedulers/universal/ourbits.py
+++ schedulers/universal/ourbits.py
@@ -20,12 +20,12 @@ SITE_NAME = "ourbits"
 #async def ourbits_seed_messges():
 #    await app.send_message(trgatid,f"祝贺OurBits八周年快乐")
 
-hour = state_manager.get_item(SITE_NAME.upper(),"hour","2,5,8,11,14,19,20,21")
-minute = state_manager.get_item(SITE_NAME.upper(),"minute","59")
-offset = state_manager.get_item(SITE_NAME.upper(),"offset",1)
-TRGATID = state_manager.get_item(SITE_NAME.upper(),"trgatid",-1001726902866) 
-@scheduler.scheduled_job("cron", hour=hour, minute=minute, second="59", id="ourbits_send_msg")
 async def ourbits_send_msg():
+    hour = state_manager.get_item(SITE_NAME.upper(),"hour","2,5,8,11,14,19,20,21")
+    minute = state_manager.get_item(SITE_NAME.upper(),"minute","59")
+    offset = state_manager.get_item(SITE_NAME.upper(),"offset",1)
+    TRGATID = state_manager.get_item(SITE_NAME.upper(),"trgatid",-1001726902866) 
+
     from app import get_user_app
     user_app = get_user_app()
     i = 0
@@ -33,3 +33,13 @@ async def ourbits_send_msg():
     while i< offset:     
         i+=1
     await user_app.send_message(TRGATID,f"祝OurBits九周年快乐")
+
+    scheduler.add_job(
+        ourbits_send_msg,
+        "cron", 
+        hour=hour, 
+        minute=minute, 
+        second="59", 
+        id="ourbits_send_msg",
+        replace_existing=True,
+    )
